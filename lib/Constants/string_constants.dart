4113ba752e2640275b562d252465de4fc7f889fb
Prithive <prithivechandrasekaran@gmail.com>
2023-04-09 00:03:26 +0530

    Otp confirmation screen design

--- lib/Constants/string_constants.dart
+++ lib/Constants/string_constants.dart
@@ -4,7 +4,13 @@ class StringConstants {
   static String otpConfirmation =
       "We will send you One-time password to this mobile number";
   static String enterMobileNumber = "Enter Mobile Number";
+  static String enterOtpSent = "Enter the OTP sent to";
+
   static String btnGetOtp = "Get OTP";
+  static String btnVerifyOtp = "Verify OTP";
+static String txtdidnotRecieveotp = "Didn’t you receive the OTP ?";
+  static String btnResendOtp = "Resend OTP";
+
 
   static String getText(String key) {
     return key;
--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import '../Constants/string_constants.dart';
 import '../Constants/color_constants.dart';
+
+enum MobileVerificationState { Mobile_SHOW_state, Otp_SHOW_state }
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,6 +14,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  MobileVerificationState currentState = MobileVerificationState.Otp_SHOW_state;
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -30,7 +34,9 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 40,
           ),
           Text(
-            StringConstants.getText(StringConstants.otpConfirmation),
+            currentState == MobileVerificationState.Mobile_SHOW_state
+                ? StringConstants.getText(StringConstants.otpConfirmation)
+                : StringConstants.getText(StringConstants.enterOtpSent),
             style: const TextStyle(
               color: ColorConstants.textColor,
               fontSize: 16,
@@ -42,7 +48,9 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 20,
           ),
           Text(
-            StringConstants.getText(StringConstants.enterMobileNumber),
+            currentState == MobileVerificationState.Mobile_SHOW_state
+                ? StringConstants.getText(StringConstants.enterMobileNumber)
+                : "",
             style: const TextStyle(
               color: ColorConstants.textColor,
               fontSize: 12,
@@ -52,23 +60,90 @@ class _LoginScreenState extends State<LoginScreen> {
           const SizedBox(
             height: 0,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: IntlPhoneField(
-              dropdownDecoration: const BoxDecoration(),
-              decoration: const InputDecoration(
-                labelText: '000-00-00-000',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(),
+          currentState == MobileVerificationState.Mobile_SHOW_state
+              ? Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: IntlPhoneField(
+                    dropdownDecoration: const BoxDecoration(),
+                    decoration: const InputDecoration(
+                      labelText: '000-00-00-000',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    initialCountryCode: 'IN',
+                    onChanged: (phone) {},
+                  ),
+                )
+              : Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Center(
+                    child: PinCodeTextField(
+                      length: 4,
+                      obscureText: false,
+                      animationType: AnimationType.fade,
+                      pinTheme: PinTheme(
+                          shape: PinCodeFieldShape.box,
+                          borderRadius: BorderRadius.circular(5),
+                          fieldHeight: 50,
+                          fieldWidth: 40,
+                          activeFillColor: ColorConstants.textColor,
+                          activeColor: ColorConstants.textColor,
+                          inactiveColor: ColorConstants.textColor),
+                      animationDuration: const Duration(milliseconds: 300),
+                      //backgroundColor: Constants.secondaryColor,
+                      enableActiveFill: false,
+                      // controller: otpController,
+                      onCompleted: (v) {
+                        debugPrint("Completed");
+                      },
+                      onChanged: (value) {
+                        debugPrint(value);
+                        setState(() {
+                          // currentText = value;
+                        });
+                      },
+                      beforeTextPaste: (text) {
+                        return true;
+                      },
+                      appContext: context,
+                    ),
+                  ),
                 ),
-              ),
-              initialCountryCode: 'IN',
-              onChanged: (phone) {},
-            ),
-          ),
           const SizedBox(
             height: 20,
           ),
+          currentState == MobileVerificationState.Otp_SHOW_state
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      StringConstants.getText(
+                          StringConstants.txtdidnotRecieveotp),
+                      style: const TextStyle(
+                        color: ColorConstants.textColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(StringConstants.getText(
+                            StringConstants.btnResendOtp)))
+                  ],
+                )
+              : Row(
+                  children: const [Text("")],
+                ),
+          currentState == MobileVerificationState.Otp_SHOW_state
+              ? const SizedBox(
+                  height: 20,
+                )
+              : const SizedBox(
+                  height: 0,
+                ),
           TextButton(
             style: TextButton.styleFrom(
               fixedSize: Size(width - 40, 50),
@@ -76,7 +151,9 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             onPressed: () {},
             child: Text(
-              StringConstants.getText(StringConstants.btnGetOtp),
+              currentState == MobileVerificationState.Mobile_SHOW_state
+                  ? StringConstants.getText(StringConstants.btnGetOtp)
+                  : StringConstants.getText(StringConstants.btnVerifyOtp),
               style: const TextStyle(
                 color: ColorConstants.buttonTxtColor,
                 fontSize: 20,
